68cae115867db8dcee25a3d0dd4b7046006d0bda
koinhug@126.com <koinhug@126.com>
2016-09-07 22:07:59 +0800

    纠错表建立

--- text_processing.py
+++ text_processing.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from pypinyin import lazy_pinyin
+import json
+
+not_need_correction = ['jv', 'jvan', 'jve', 'jvn',
+                       'qv', 'qvan', 'qve', 'qvn',
+                       'xv', 'xvan', 'xve', 'xvn']
+
+def writejson2file(data, filename):
+    with open(filename, 'w') as outfile:
+        data = json.dumps(data, indent=4, sort_keys=True)
+        outfile.write(data)
+
+def readjson(filename):
+    with open(filename) as outfile:
+        return json.load(outfile)
 
 def generator_chinese():
     for line in open('./data/input/ref_word.txt'):
@@ -38,9 +52,77 @@ def generator_true_pinyin():
             pinyin_true_file.write(pinyin_line_out)
             pinyin_true_file.write('\n')
 
+def generator_finally():
+    for line in open('./data/output/chinese.txt'):
+        line = line.replace('\n', ' ')
+        with open('./data/output/chinese_finally.txt', 'a') as chinese_finally_file:
+            chinese_finally_file.write(line)
 
+    for line in open('./data/output/pinyin_error.txt'):
+        line = line.replace('\n', ' ')
+        with open('./data/output/pinyin_error_finally.txt', 'a') as pinyin_error_finally_file:
+            pinyin_error_finally_file.write(line)
 
+    for line in open('./data/output/pinyin_true.txt'):
+        line = line.replace('\n', ' ')
+        with open('./data/output/pinyin_true_finally.txt', 'a') as pinyin_true_finally_file:
+            pinyin_true_finally_file.write(line)
 
+def generator_error_statistics():
+    with open('./data/output/pinyin_error_finally.txt') as pinyin_error_finally_file:
+        pinyin_error_content = pinyin_error_finally_file.read()
+        pinyin_error_list = pinyin_error_content.split(' ')
 
+    with open('./data/output/pinyin_true_finally.txt') as pinyin_true_file:
+        pinyin_true_content = pinyin_true_file.read()
+        pinyin_true_list = pinyin_true_content.split(' ')
 
+    true2error_statistics = {}
+    error2true_statistics = {}
+    for i in range(0, len(pinyin_error_list)):
+        if pinyin_error_list[i] != pinyin_true_list[i] and pinyin_error_list[i] not in not_need_correction:
+            true2error_statistics.setdefault(pinyin_true_list[i], {})
+            true2error_statistics[pinyin_true_list[i]].setdefault(pinyin_error_list[i], 0)
+            true2error_statistics[pinyin_true_list[i]][pinyin_error_list[i]] += 1
 
+            error2true_statistics.setdefault(pinyin_error_list[i], {})
+            error2true_statistics[pinyin_error_list[i]].setdefault(pinyin_true_list[i], 0)
+            error2true_statistics[pinyin_error_list[i]][pinyin_true_list[i]] += 1
+
+    writejson2file(true2error_statistics, './data/output/true2error_statistics.json')
+    writejson2file(error2true_statistics, './data/output/error2true_statistics.json')
+
+correction_table = {}
+correction_table.setdefault('aaa', 'a')
+correction_table.setdefault('ix', 'i')
+correction_table.setdefault('eee', 'e')
+correction_table.setdefault('iiiu', 'you')
+correction_table.setdefault('iiio', 'yo')
+correction_table.setdefault('iiin', 'yin')
+correction_table.setdefault('iiie', 'ye')
+correction_table.setdefault('iiia', 'ya')
+correction_table.setdefault('iii', 'yi')
+correction_table.setdefault('ooo', 'o')
+correction_table.setdefault('iz', 'i')
+correction_table.setdefault('uuun', 'wen')
+correction_table.setdefault('uuui', 'wei')
+correction_table.setdefault('uuu', 'u')
+correction_table.setdefault('aaa', 'a')
+correction_table.setdefault('vvv', 'yu')
+correction_table_special_keys = correction_table.keys()
+
+error2true_statistics = readjson('./data/output/error2true_statistics.json')
+
+for key in error2true_statistics:
+    if key not in correction_table_special_keys:
+        correction_table.setdefault(key, list(error2true_statistics[key].keys()))
+
+writejson2file(correction_table, './data/output/correction_table.json')
+
+if __name__ == '__main__':
+    # generator_chinese()
+    # generator_error_pinyin()
+    # generator_true_pinyin()
+    # generator_finally()
+    # generator_error_statistics()
+    pass
